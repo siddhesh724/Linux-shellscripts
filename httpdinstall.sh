ae992d86aa42800a1824e0abe33a24e74adc2c4e
root <root@ip-172-31-95-140.ec2.internal>
2023-01-03 09:42:13 +0000

    syntax corrected and change user to root

--- httpdinstall.sh
+++ httpdinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo yum install httpd -y
-sudo echo "this contain from shell scripts" >> /var/www/html/index.html
-sudo systemctl restart httpd
-sudo systemctl enable httpd
+echo "this contain from shell scripts" >> /var/www/html/index.html
+systemctl restart httpd
+systemctl enable httpd
