ea21bbac4d0cb56a29fe90ba12d73c3f8fcd73f1
siddhesh724 <siddheshtendulkar724@gmail.com>
2023-01-06 17:01:35 +0530

    backup script ready and testing done working fine

--- backup.sh
+++ backup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "enter source path"
-read PATH
+read sourcepath
 echo "enter source Bucket name"
-read BUCKET
-aws s3 cp $PATH s3://$BUCKET/backup/ --recursive
+read bucket
+aws s3 cp $sourcepath s3://$bucket/backup/ --recursive
+if [ $? -eq 0 ]
+    then 
+    echo "backup sucessful"
+    else
+    echo "backup unsucessful"
+fi
